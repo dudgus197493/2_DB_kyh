3fe1376b6cbddaed12265425f4c2cfacabfd1703
Tonic Kim <79572904+dudgus197493@users.noreply.github.com>
2022-09-08 16:50:47 +0900

    09/08 과제3번(Option)파일 15번까지 풀이

--- 과제/SQL03_SELECT(Option).sql
+++ 과제/SQL03_SELECT(Option).sql
@@ -68,7 +68,79 @@ NATURAL JOIN TB_PROFESSOR
 
 -- 9. 8번의 결과 중 '인문사회' 계열에 속한 과목의 교수 이름을 찾으려고 한다.
 -- 이에 해당하는 과목 이름과 교수 이름을 출력하는 SQL문을 작성하시오.
-SELECT CLASS_
+SELECT CLASS_NAME, PROFESSOR_NAME
 FROM TB_CLASS
+JOIN TB_CLASS_PROFESSOR USING(CLASS_NO)
+JOIN TB_PROFESSOR P USING(PROFESSOR_NO)
+JOIN TB_DEPARTMENT D ON (P.DEPARTMENT_NO = D.DEPARTMENT_NO)
+WHERE CATEGORY = '인문사회';
+
+-- 10. '음악학과' 학생들의 평점을 구하려고 한다. 
+-- 음악학과 학생들의 "학번", "학생 이름", "전체 평점"을 출력하는 SQL 문장을 작성하시오.
+-- (단, 평점은 소수점 1자리까지만 반올림하여 표시)
+
+SELECT S.STUDENT_NO 학번, S.STUDENT_NAME "학생 이름", ROUND(AVG(POINT), 1)
+FROM TB_STUDENT S
+JOIN TB_DEPARTMENT D ON (S.DEPARTMENT_NO = D.DEPARTMENT_NO)
+JOIN TB_GRADE G ON (S.STUDENT_NO = G.STUDENT_NO)
+WHERE DEPARTMENT_NAME = '음악학과'
+GROUP BY S.STUDENT_NAME, S.STUDENT_NO
+ORDER BY 1;
+
+-- 11. 학번이 A313047 인 학생이 학교에 나오고 있지 않다.
+-- 지도 교수에게 내용을 전달하기 위한 학과 이름, 학생 이름과 지도 교수 이름이 필요하다.
+-- 이때 사용할 SQL문을 작성하시오. 단, 출력헤더는 "학과이름", "학생이름", "지도교수이름" 으로 출력
+SELECT D.DEPARTMENT_NAME 학과이름 ,S.STUDENT_NAME 학생이름, P.PROFESSOR_NAME 지도교수이름
+FROM TB_STUDENT S
+JOIN TB_DEPARTMENT D ON (S.DEPARTMENT_NO = D.DEPARTMENT_NO)
+JOIN TB_PROFESSOR P ON (S.COACH_PROFESSOR_NO = P.PROFESSOR_NO)
+WHERE S.STUDENT_NO = 'A313047';
+
+-- 12. 2007 년도에 '인간관계론' 과목을 수강한 학생을 찾아 학생이름과 수강학기를 표시하는 SQL 문장을 작성하시오.
+
+SELECT S.STUDENT_NAME, G.TERM_NO TERM_NAME
+FROM TB_STUDENT S
+JOIN TB_GRADE G ON (S.STUDENT_NO = G.STUDENT_NO)
+JOIN TB_CLASS C ON (G.CLASS_NO = C.CLASS_NO)
+WHERE G.TERM_NO LIKE '2007%'
+AND C.CLASS_NAME = '인간관계론';
+
+-- 13. 예체능 계열 과목 중 과목 담당교수를 한 명도 배정받지 못한 과목을 찾아
+-- 그 과목 이름과 학과 이름을 출력하는 SQL문장을 작성하시오.
+SELECT C.CLASS_NAME, D.DEPARTMENT_NAME
+FROM TB_CLASS C
+LEFT JOIN TB_CLASS_PROFESSOR CP ON (C.CLASS_NO = CP.CLASS_NO)
+JOIN TB_DEPARTMENT D ON (C.DEPARTMENT_NO = D.DEPARTMENT_NO)
+WHERE D.CATEGORY = '예체능'
+AND CP.PROFESSOR_NO IS NULL;
+
+-- 14. 춘 기술대학교 서반아어학과 학생들의 지도교수를 게시하고자 한다.
+-- 학생이름과 지도교수 이름을 찾고 만일 지도 교수가 없는 학생일 경우
+-- "지도교수 미지정" 으로 표시하도록 하는 SQL문을 작성하시오.
+-- 단, 출력헤더는 "학생이름", "지도교수"로 표시하며 고학번 학생이 먼저 표시되도록 한다.
+SELECT STUDENT_NAME 학생이름, NVL(PROFESSOR_NAME, '지도교수 미지정') 지도교수
+FROM TB_STUDENT S
+JOIN TB_DEPARTMENT D ON (S.DEPARTMENT_NO = D.DEPARTMENT_NO)
+LEFT JOIN TB_PROFESSOR P ON (S.COACH_PROFESSOR_NO = P.PROFESSOR_NO)
+WHERE D.DEPARTMENT_NAME = '서반아어학과'
+ORDER BY S.STUDENT_NO ASC;
+
+-- 15. 휴학생이 아닌 학생 중 평점이 4.0 이상인 학생을 찾아
+-- 그 학생의 학번, 이름, 학과이름, 평점을 출력하는 SQL 문을 작성하시오.
+SELECT S.STUDENT_NO 학번, S.STUDENT_NAME 이름, D.DEPARTMENT_NAME "학과 이름", AVG(G.POINT) 평점
+FROM TB_STUDENT S
+JOIN TB_DEPARTMENT D ON (S.DEPARTMENT_NO = D.DEPARTMENT_NO)
+JOIN TB_GRADE G ON (S.STUDENT_NO = G.STUDENT_NO)
+WHERE S.ABSENCE_YN = 'N'
+GROUP BY S.STUDENT_NO, S.STUDENT_NAME, D.DEPARTMENT_NAME
+HAVING AVG(G.POINT) >= 4
+ORDER BY 1;
+
+-- 16. 환경조경학과 전공과목들의 과목 별 평점을 파악할 수 있는 SQL문을 작성하시오.
+
+SELECT * FROM TB_STUDENT WHERE STUDENT_NAME = '김소라'
+
+SELECT * FROM TB_STUDENT
+
 
 
